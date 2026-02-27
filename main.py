126f57164e9c3f1bf26d80baf4c3ed85d60497cc
Evgeny Volkov <volkov22dla@yandex.ru>
2026-02-27 17:11:46 +0300

    Enable keypoint detection and visualization in video frames

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ if not cap.isOpened():
 
 save_path = "data/frames/"
 save_flag = False
-show_flag = False
+show_flag = True
+orb = cv2.ORB_create()
 
 i = 0
 while cap.isOpened():
@@ -19,7 +20,11 @@ while cap.isOpened():
 
     if show_flag:
         frame = cv2.resize(frame, (int(frame.shape[1]*0.5), int(frame.shape[0]*0.5)))
-        cv2.imshow("Frame", frame)
+        kp = orb.detect(frame, None)
+        kp, des = orb.compute(frame, kp)
+        img2 = cv2.drawKeypoints(frame, kp, None, color=(0,255,0), flags=0)
+
+        cv2.imshow("Frame", img2)
 
     if cv2.waitKey(25) & 0xFF == ord("q"):
         break
